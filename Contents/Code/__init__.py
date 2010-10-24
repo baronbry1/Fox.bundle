dc8b6a1b79ed74c1583c6fc160c0f9f1b2c00e0a
sander1 <sanderspies+plexapp@gmail.com>
2010-10-24 23:59:35 +0200

    Upgrade to v2

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,90 +1,63 @@
 import re, random
-from PMS import *
-from PMS.Objects import *
-from PMS.Shortcuts import *
-##################################################################################################FOX
-PLUGIN_PREFIX     = "/video/Fox"
 
+##################################################################################################FOX
+
+PLUGIN_PREFIX               = "/video/fox"
 FOX_URL                     = "http://www.fox.com"
 FOX_FULL_EPISODES_SHOW_LIST = "http://www.fox.com/full-episodes/"
-CACHE_INTERVAL              = 3600
 FOX_FEED                    = "http://www.fox.com/fod"
-DEBUG                       = False
-foxart                      ="art-default.png"
-foxthumb                    ="icon-default.png"
+FOX_ART                     = "art-default.jpg"
+FOX_THUMB                   = "icon-default.png"
 
 ####################################################################################################
-
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "FOX","icon-default.png", "art-default.png")
+  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "FOX", FOX_ART, FOX_THUMB)
   Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-  
-  MediaContainer.art        =R(foxart)
-  DirectoryItem.thumb       =R(foxthumb)
-  WebVideoItem.thumb        =R(foxthumb)
 
-####################################################################################################
-#def MainMenu():
-#    dir = MediaContainer(mediaType='video') 
-#    dir.Append(Function(DirectoryItem(all_shows, "All Shows"), pageUrl = #FOX_FULL_EPISODES_SHOW_LIST))
-#    return dir
-    
+  MediaContainer.art        = R(FOX_ART)
+  DirectoryItem.thumb       = R(FOX_THUMB)
+  WebVideoItem.thumb        = R(FOX_THUMB)
+
+  HTTP.CacheTime = CACHE_1HOUR
+  HTTP.Headers['User-agent'] = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11"
+
 ####################################################################################################
 def MainMenu():
     dir = MediaContainer(mediaType='video')
-#    dir = MediaContainer(title2=sender.itemTitle)
-    pageUrl=FOX_FULL_EPISODES_SHOW_LIST
-    content = XML.ElementFromURL(pageUrl, True)
-    Log(content.xpath('//div[@class="showInfo"]'))
+    content = HTML.ElementFromURL(FOX_FULL_EPISODES_SHOW_LIST)
+
     for item in content.xpath('//li[@class="episodeListItem"]/div[@class="showInfo"]'):
+      titles = item.xpath("a")
+      titleUrl = FOX_URL + titles[1].get('href')
+      title = item.xpath("h3")[0].text
+      summary = item.xpath("h4")[0].text
 
-      titles=item.xpath("a")
-      Log(titles)
-      titleUrl=FOX_URL + titles[1].get('href')
-      Log(titleUrl)
-
-      Log(titleUrl)
-      title=item.xpath("h3")[0].text
-      summary=item.xpath("h4")[0].text
-      thumb=""
-      Log(thumb)
-
-      
-      
-      Log(titleUrl)
-      if (titleUrl.count('americasmostwanted')) ==0:
-        dir.Append(Function(DirectoryItem(VideoPage, title,summary), pageUrl = titleUrl))
+      if (titleUrl.count('americasmostwanted')) == 0:
+        dir.Append(Function(DirectoryItem(VideoPage, title, summary), pageUrl=titleUrl))
     return dir 
 
 ####################################################################################################
 def VideoPage(sender, pageUrl):
     dir = MediaContainer(title2=sender.itemTitle)
-    Log(pageUrl)
-    content = XML.ElementFromURL(pageUrl, isHTML="True")
+    content = HTML.ElementFromURL(pageUrl)
+
     for item2 in content.xpath('//ul[@id="fullEpisodesList"]/li[contains(@class,"episode")]/ul'):
-
-
-      vidUrl=FOX_URL+item2.xpath('li[@class="episodeName"]/span/a')[0].get('href')
-      Log(vidUrl)
+      vidUrl = FOX_URL + item2.xpath('li[@class="episodeName"]/span/a')[0].get('href')
       title2 = item2.xpath('li[@class="episodeName"]/span/a')[0].text
-      Log(title2)
-      Log(item2.xpath('li[@class="episodeNumber"]')[0].text)
       title1 = item2.xpath('li[@class="episodeName"]/span/a')[0].text
-      summary=item2.xpath('li[@class="description"]')[0].get('href')
-      airdate=item2.xpath('li[@class="airDate"]')[0].text
-      title2=title2 + " - " + airdate
-      
-      
-      content2 = XML.ElementFromURL(vidUrl,True)
-      pageUrl2=pageUrl
-      pageUrl2=pageUrl2.replace("/","%2F")
-      pageUrl2=pageUrl2.replace(":","%3A")
-      pageUrl2=pageUrl2+"&%40"
-      
+      summary = item2.xpath('li[@class="description"]')[0].get('href')
+      airdate = item2.xpath('li[@class="airDate"]')[0].text
+      title2 = title2 + " - " + airdate
+
+      content2 = HTML.ElementFromURL(vidUrl)
+      pageUrl2 = pageUrl
+      pageUrl2 = pageUrl2.replace("/","%2F")
+      pageUrl2 = pageUrl2.replace(":","%3A")
+      pageUrl2 = pageUrl2 + "&%40"
 
       videoID = content2.xpath('//div[@id="player"]//object/param[@name="@videoPlayer"]')[0].get("value")
       bgcolor = content2.xpath('//div[@id="player"]//object/param[@name="bgcolor"]')[0].get("value")
-      bgcolor=bgcolor.replace("#","%23")
+      bgcolor = bgcolor.replace("#","%23")
       width = content2.xpath('//div[@id="player"]//object/param[@name="width"]')[0].get("value")
       height = content2.xpath('//div[@id="player"]//object/param[@name="height"]')[0].get("value")
       playerID = content2.xpath('//div[@id="player"]//object/param[@name="playerID"]')[0].get("value")
@@ -99,12 +72,8 @@ def VideoPage(sender, pageUrl):
       optimizedContentLoad = content2.xpath('//div[@id="player"]//object/param[@name="optimizedContentLoad"]')[0].get("value")
       convivaEnabled = content2.xpath('//div[@id="player"]//object/param[@name="convivaEnabled"]')[0].get("value")
       convivaID = content2.xpath('//div[@id="player"]//object/param[@name="convivaID"]')[0].get("value")
-      
 
-      
-      truevidUrl="http://admin.brightcove.com/viewer/us1.24.00.06a/BrightcoveBootloader.swf?purl=" + pageUrl2 + "videoPlayer=" + videoID + "&adZone=" + adZone + "&autoStart=true" + "&bgcolor=" + bgcolor + "&convivaEnabled=" + convivaEnabled + "&convivaID=" + convivaID + "&dynamicStreaming=" + dynamicStreaming + "&flashID=myExperience" + "&height=" + height + "&isVid=" + isVid + "&omnitureAccountID=" + omnitureAccountID + "&optimizedContentLoad=" + optimizedContentLoad + "&playerID=" + playerID + "&publisherID=" + publisherID + "&showcode=" + showCode + "&width=" + width +"&wmode=" + wmode
-      
-      Log(truevidUrl)
+      truevidUrl = "http://admin.brightcove.com/viewer/us1.24.00.06a/BrightcoveBootloader.swf?purl=" + pageUrl2 + "videoPlayer=" + videoID + "&adZone=" + adZone + "&autoStart=true" + "&bgcolor=" + bgcolor + "&convivaEnabled=" + convivaEnabled + "&convivaID=" + convivaID + "&dynamicStreaming=" + dynamicStreaming + "&flashID=myExperience" + "&height=" + height + "&isVid=" + isVid + "&omnitureAccountID=" + omnitureAccountID + "&optimizedContentLoad=" + optimizedContentLoad + "&playerID=" + playerID + "&publisherID=" + publisherID + "&showcode=" + showCode + "&width=" + width +"&wmode=" + wmode
 
-      dir.Append(WebVideoItem(truevidUrl, title=title2, subtitle=title1,summary=summary))
+      dir.Append(WebVideoItem(truevidUrl, title=title2, subtitle=title1, summary=summary))
     return dir
