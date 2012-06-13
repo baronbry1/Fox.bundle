dd2a3e8540a8446bacf7b73e8d00e012edf6b141
sander1 <sander@plexapp.com>
2012-06-13 16:57:11 +0200

    Remove unused var(s); add PlexClientCapabilities to plist; change indentation.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,72 +1,75 @@
 PLUGIN_PREFIX               = "/video/fox"
 FOX_URL                     = "http://www.fox.com"
 FOX_FULL_EPISODES_SHOW_LIST = "http://www.fox.com/full-episodes/"
-FOX_FEED                    = "http://www.fox.com/fod"
 FOX_ART                     = "art-default.jpg"
 FOX_THUMB                   = "icon-default.png"
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "FOX", FOX_THUMB, FOX_ART)
 
-  ObjectContainer.art         = R(FOX_ART)
-  ObjectContainer.title1      = "FOX"
-  DirectoryObject.thumb       = R(FOX_THUMB)
-  
-  HTTP.CacheTime = CACHE_1HOUR
-  HTTP.Headers['User-agent'] = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11"
+	Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "FOX", FOX_THUMB, FOX_ART)
+
+	ObjectContainer.art = R(FOX_ART)
+	ObjectContainer.title1 = "FOX"
+	DirectoryObject.thumb = R(FOX_THUMB)
+
+	HTTP.CacheTime = CACHE_1HOUR
+	HTTP.Headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:12.0) Gecko/20100101 Firefox/12.0"
 
 ####################################################################################################
 def MainMenu():
-    oc = ObjectContainer()
-    content = HTML.ElementFromURL(FOX_FULL_EPISODES_SHOW_LIST)
 
-    for item in content.xpath('//li[@class="episodeListItem"]/div[@class="showInfo"]'):
-        titles = item.xpath("a")
-        link = titles[1].get('href')
-        if link.startswith('http'):
-            titleUrl = link
-        elif link == '':
-            continue
-        else:
-            titleUrl = FOX_URL + link
-        title = item.xpath("h3")[0].text
-        summary = item.xpath("h4")[0].text
+	oc = ObjectContainer()
+	content = HTML.ElementFromURL(FOX_FULL_EPISODES_SHOW_LIST)
 
-        if (titleUrl.count('americasmostwanted')) == 0:
-            oc.add(DirectoryObject(key=Callback(VideoPage, pageUrl=titleUrl, title=title), title=title, summary=summary))
-    return oc
+	for item in content.xpath('//li[@class="episodeListItem"]/div[@class="showInfo"]'):
+		titles = item.xpath("a")
+		link = titles[1].get('href')
+		if link.startswith('http'):
+			titleUrl = link
+		elif link == '':
+			continue
+		else:
+			titleUrl = FOX_URL + link
+		title = item.xpath("h3")[0].text
+		summary = item.xpath("h4")[0].text
+
+		if (titleUrl.count('americasmostwanted')) == 0:
+			oc.add(DirectoryObject(key=Callback(VideoPage, pageUrl=titleUrl, title=title), title=title, summary=summary))
+
+	return oc
 
 ####################################################################################################
 def VideoPage(pageUrl, title):
-    oc = ObjectContainer(title2=title)
-    content = HTML.ElementFromURL(pageUrl)
 
-    for episode in content.xpath('//ul[@id="fullEpisodesList"]/li[contains(@class,"episode")]'):
-        details = JSON.ObjectFromString(episode.xpath('.//script[@class="videoObject"]')[0].text)
-        id = str(details['id'])
-        episode_html = content.xpath('//li[@data-video-id="'+id+'"]')[0]
-        locked = episode_html.xpath('.//span[@class="playerStatus padlock"]')
-        if len(locked) > 0:
-            Log("Episode Locked. Skipping")
-            continue
-        else:
-            pass
-        episode_title = details['name']
-        summary = details['shortDescription']
-        thumbs = [details['videoStillURL'], details['thumbnailURL']]
-        if pageUrl.endswith('/'):
-            video_url = pageUrl + '%s/' % details['id']
-        else:
-            video_url = pageUrl + '/%s/' % details['id']
-        duration = int(details['length'])*1000
-        index = int(details['episode'])
-        season = int(details['season'])
-        rating = details['rating']
-        date = Datetime.ParseDate(details['airdate']).date()
-      
-        oc.add(EpisodeObject(url=video_url, title=episode_title, show=title, summary=summary, index=index,
-            season=season, duration=duration, content_rating=rating, originally_available_at=date,
-            thumb=Resource.ContentsOfURLWithFallback(url=thumbs, fallback=FOX_THUMB)))
-            
-    return oc
+	oc = ObjectContainer(title2=title)
+	content = HTML.ElementFromURL(pageUrl)
+
+	for episode in content.xpath('//ul[@id="fullEpisodesList"]/li[contains(@class,"episode")]'):
+		details = JSON.ObjectFromString(episode.xpath('.//script[@class="videoObject"]')[0].text)
+		id = str(details['id'])
+		episode_html = content.xpath('//li[@data-video-id="'+id+'"]')[0]
+		locked = episode_html.xpath('.//span[@class="playerStatus padlock"]')
+		if len(locked) > 0:
+			Log("Episode Locked. Skipping")
+			continue
+		else:
+			pass
+		episode_title = details['name']
+		summary = details['shortDescription']
+		thumbs = [details['videoStillURL'], details['thumbnailURL']]
+		if pageUrl.endswith('/'):
+			video_url = pageUrl + '%s/' % details['id']
+		else:
+			video_url = pageUrl + '/%s/' % details['id']
+		duration = int(details['length'])*1000
+		index = int(details['episode'])
+		season = int(details['season'])
+		rating = details['rating']
+		date = Datetime.ParseDate(details['airdate']).date()
+
+		oc.add(EpisodeObject(url=video_url, title=episode_title, show=title, summary=summary, index=index,
+			season=season, duration=duration, content_rating=rating, originally_available_at=date,
+			thumb=Resource.ContentsOfURLWithFallback(url=thumbs, fallback=FOX_THUMB)))
+
+	return oc
